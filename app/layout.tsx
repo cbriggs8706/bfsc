026df654e4cfe9ecc2bb878f3c8a71c91b8ceaf5
cbriggs8706 <cameronbriggs8706@gmail.com>
2026-02-28 20:18:03 -0700

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 // app/layout.tsx
+import type { Metadata } from 'next'
 import './globals.css'
+
+export const metadata: Metadata = {
+	icons: {
+		icon: [
+			{ url: '/favicon.ico', sizes: 'any' },
+			{ url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+			{ url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+		],
+		apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+	},
+	manifest: '/site.webmanifest',
+}
 
 export default function RootLayout({
 	children,
